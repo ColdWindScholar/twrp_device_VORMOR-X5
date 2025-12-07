3390830155d0ad85dab2108dcc9e2903e12994b7
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2025-12-08 00:04:47 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_VORMOR-X5-user
-add_lunch_combo omni_VORMOR-X5-userdebug
-add_lunch_combo omni_VORMOR-X5-eng
+add_lunch_combo omni_VORMOR_X5-user
+add_lunch_combo omni_VORMOR_X5-userdebug
+add_lunch_combo omni_VORMOR_X5-eng
