418469a25edfc46c5603c8773c8d48333b10c101
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2025-12-07 23:54:43 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/rockchip/VORMOR-X5
+DEVICE_PATH := device/XueZhiYou/VORMOR-X5
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
